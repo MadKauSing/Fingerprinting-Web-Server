e36197ebfe10dc29d12769e4446ffb89a5f5a280
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-04-19 19:00:19 +0530

    Added URL inputs via command line arguments

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import re
 from urllib import response
 import requests
-import httplib2
-req = requests.get('http://www.cloudflare.com')
-headers = ['Server', 'Date','Last-Modified','ETag','Content-Length','Accept-Ranges','Connection','Content-Type']
-print(req.raw.version/10)
-print(req.status_code)
-for header in headers:
-    try:
+import sys
+
+def fingerprint_web_server(argv):
+    req = requests.get('http://'+argv[1])
+    headers = ['Server', 'Date','Last-Modified','ETag','Accept-Ranges','Content-Length','Connection','Content-Type','Content-Encoding']
+    print("\nHTTP/%.1f %d \n"%(req.raw.version/10,req.status_code))
+    # print(req.status_code)
+    for header in headers:
+        try:
+            result = req.headers[header]
+            print('%s: %s' % (header, result))
+        except Exception as error:
+            continue
+    for header in req.headers:
         result = req.headers[header]
-        print('%s: %s' % (header, result))
-    except Exception as error:
-        continue
+        if(header.startswith('X-') or header.startswith('x-')):
+            print('%s: %s' % (header, result))
 
-print("\nCustom headers used by the website: \n")
-
-for header in req.headers:
-    result = req.headers[header]
-    if(header.startswith('X-')):
-        print('%s: %s' % (header, result))
+if __name__ == "__main__":
+    try:
+        fingerprint_web_server(sys.argv)
+    except:
+        print("An error occured... Try a different URL")
