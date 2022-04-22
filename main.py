2702f60f20325005d9fc586bd857b9132dd86faf
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-04-22 05:38:41 +0530

    Added a -a argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import re
-from urllib import response
 import requests
 import sys
 
@@ -7,17 +6,21 @@ def fingerprint_web_server(argv):
     req = requests.get('http://'+argv[1])
     headers = ['Server', 'Date','Last-Modified','ETag','Accept-Ranges','Content-Length','Connection','Content-Type','Content-Encoding']
     print("\nHTTP/%.1f %d \n"%(req.raw.version/10,req.status_code))
-    # print(req.status_code)
-    for header in headers:
-        try:
+    if(len(argv) == 3 and argv[2] == '-a'):
+        for header in req.headers:
             result = req.headers[header]
             print('%s: %s' % (header, result))
-        except Exception as error:
-            print("%s : Not found"%(header))
-    for header in req.headers:
-        result = req.headers[header]
-        if(header.startswith('X-') or header.startswith('x-')):
-            print('%s: %s' % (header, result))
+    else:
+        for header in headers:
+            try:
+                result = req.headers[header]
+                print('%s: %s' % (header, result))
+            except Exception as error:
+                print("%s : Not found"%(header))
+        for header in req.headers:
+            result = req.headers[header]
+            if(header.startswith('X-') or header.startswith('x-')):
+                print('%s: %s' % (header, result))
 
 if __name__ == "__main__":
     try:
