a33cb1c95eba37ef14443431987919e3e408dfb2
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-04-25 14:24:07 +0530

    Final commit

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import requests
 import sys
 
 def fingerprint_web_server(argv):
-    req = requests.get('http://'+argv[1])
     headers = ['Server', 'Date','Last-Modified','ETag','Accept-Ranges','Content-Length','Connection','Content-Type','Content-Encoding']
-    print("\nHTTP/%.1f %d \n"%(req.raw.version/10,req.status_code))
-    if(len(argv) == 3 and argv[2] == '-a'):
+    if(len(argv) == 3 and argv[1] == '-a'):
+        req = requests.get('http://'+argv[2])
+        print("\nHTTP/%.1f %d \n"%(req.raw.version/10,req.status_code))
         for header in req.headers:
             result = req.headers[header]
             print('%s: %s' % (header, result))
+    elif(len(argv) == 4 and argv[1] == '-s'):
+        req1 = requests.get('http://'+argv[2])
+        req2 = requests.get('http://'+argv[3])
+        intersection = dict(req1.headers.items() & req2.headers.items() )
+        for header in intersection:
+            result = req1.headers[header]
+            print("%s : %s" % (header,result))
     else:
+        req = requests.get('http://'+argv[1])
+        print("\nHTTP/%.1f %d \n"%(req.raw.version/10,req.status_code))
         for header in headers:
             try:
                 result = req.headers[header]
